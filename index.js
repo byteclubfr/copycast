cd9ac5c1a09efc8a8cf2100b19d6326a2f63f3eb
Delapouite <delapouite@gmail.com>
2016-01-25 19:41:09 +0100

    refactor(server): move http related fns to own file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var os = require('os')
-var fs = require('fs')
-var server = require('http').createServer()
+// TODO get from argv
+const PATH = '.'
+const PORT = 42000
+
+var http = require('./server/http')
+var server = http.createServer()
 var io = require('socket.io')(server)
 
 var getWatcher = require('./server/watcher').getWatcher
-
-const PATH = '.'
 
 // state
 var tree = {
@@ -18,39 +19,8 @@ var broadcastTree = (tree) => io.emit('tree', tree)
 
 getWatcher(PATH, tree, broadcastTree)
 
-// http utils
-var sendFile = (res, name, mime) => {
-	res.writeHead(200, {'Content-Type': mime})
-	fs.readFile(`client${name}`, 'utf8', (err, content) => {
-		if (err) throw err
-		res.end(content)
-	})
-}
-
-// http
-const displayAddresses = () => {
-	const interfaces = os.networkInterfaces()
-	Object.keys(interfaces).forEach((dev) => {
-		interfaces[dev].forEach((details) => {
-			if (details.family !== 'IPv4') return
-			console.log('http://' + details.address + ':' + PORT)
-		})
-	})
-}
-const clientFiles = {
-	'/index.html': 'text/html',
-	'/index.css': 'text/css',
-	'/bundle.js': 'application/javascript'
-}
-const PORT = 42000
-
-server.on('request', (req, res) => {
-	var url = req.url === '/' ? '/index.html' : req.url
-	if (clientFiles[url]) sendFile(res, url, clientFiles[url])
-	// else res.end()
-})
 server.listen(PORT)
-displayAddresses()
+http.displayAddresses(PORT)
 
 // socket
 io.on('connection', (socket) => {
---
+++ server/http.js
@@ -0,0 +1,40 @@
+var fs = require('fs')
+var os = require('os')
+var http = require('http')
+
+const clientFiles = {
+	'/index.html': 'text/html',
+	'/index.css': 'text/css',
+	'/bundle.js': 'application/javascript'
+}
+
+// utils
+
+const sendFile = (res, name, mime) => {
+	res.writeHead(200, {'Content-Type': mime})
+	fs.readFile(`client${name}`, 'utf8', (err, content) => {
+		if (err) throw err
+		res.end(content)
+	})
+}
+
+const displayAddresses = (port) => {
+	const interfaces = os.networkInterfaces()
+	Object.keys(interfaces).forEach((dev) => {
+		interfaces[dev].forEach((details) => {
+			if (details.family !== 'IPv4') return
+			console.log('http://' + details.address + ':' + port)
+		})
+	})
+}
+
+exports.createServer = () => {
+	const server = http.createServer()
+	server.on('request', (req, res) => {
+		var url = req.url === '/' ? '/index.html' : req.url
+		if (clientFiles[url]) sendFile(res, url, clientFiles[url])
+		// else res.end()
+	})
+	return server
+}
+exports.displayAddresses = displayAddresses
