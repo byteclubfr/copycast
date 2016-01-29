14c0a468f3b79e8cd7d5f8865c3711a51a3bce1d
Delapouite <delapouite@gmail.com>
2016-01-29 12:17:32 +0100

    feat(client): add json hl

--- client/hl.js
+++ client/hl.js
@@ -6,7 +6,7 @@ import { highlightAuto } from 'highlight.js'
 // used to help hightlight.js guessing the content of the parsed code
 // beware, if content is markdown, it can start with a #
 // which will be interpreted as a CSS id selector by virtual-dom
-const langs = ['javascript', 'css', 'html', 'markdown']
+const langs = ['javascript', 'json', 'css', 'html', 'markdown']
 
 const convertHTML = HTMLToVdom({ VNode: VNode, VText })
 export default (str) => convertHTML(highlightAuto(str || '', langs).value || '<noscript />')
