7fe0a1271e5835f1f3111c218fd117bedd375a5b
Delapouite <delapouite@gmail.com>
2016-02-02 14:54:03 +0100

    refactor(client): move components to own file

---
+++ client/components.js
@@ -0,0 +1,94 @@
+import {
+	a, aside, code, div, footer, header, h1, h2,
+	li, option, pre, select, span, ul
+} from '@cycle/dom'
+import { PATH_SEP } from './utils/tree-walker'
+import { hl, hlThemes } from './hl'
+
+// protection for files with UTF-8 chars like ❭ in this one
+const toDataUri = (content) =>
+	`data:text/plain;base64,${btoa(unescape(encodeURIComponent(content)))}`
+
+
+const Octicon = (name) => span(`.octicon.octicon-${name}`)
+
+export const Sidebar = ({ tree, selected, collapsed, conn, hlTheme, sidebarWidth }) =>
+	aside('.sidebar', { style: { width: `${sidebarWidth}px` } }, [
+		h1('.logo', a({ href: 'https://github.com/lmtm/copycast' }, 'copycast')),
+		div('.tree',
+			Dir({ root: true, path: tree.name, tree, selected, collapsed })
+		),
+		Footer({ conn, hlTheme })
+	])
+
+const Dir = ({ root, path, tree, selected, collapsed }) => {
+	if (!tree || !tree.children) return
+
+	path = root ? tree.name : `${path}${PATH_SEP}${tree.name}`
+	let klass = '.dirname'
+	let trees = []
+	if (collapsed.has(path)) {
+		klass += '.collapsed'
+	} else {
+		trees = tree.children.map((child) => {
+			return (child.children)
+				? Dir({ path, tree: child, selected, collapsed })
+				: File({ path, file: child, selected })
+		})
+	}
+	return ul('.dir', [li(klass, { data: { id: path } }, tree.name), ...trees])
+}
+
+const File = ({ path, file, selected }) => {
+	const { name, updatedAt } = file
+	const id = `${path}${PATH_SEP}${name}`
+	let elapsed = ''
+	if (updatedAt) {
+		let ago = Math.round((Date.now() - updatedAt) / 1000)
+		if (ago <= 180) {
+			elapsed = `${ago}s`
+		}
+	}
+	return li(`.file${ selected === id ? '.selected' : '' }`, [
+		span('.filename', { data: { id } }, name),
+		span('.elapsed', { data: { id } }, elapsed)
+	])
+}
+
+const Footer = ({ conn, hlTheme }) =>
+	footer([
+		div('.status', [
+			span(`.conn-${ conn ? 'on' : 'off' }`, { title: 'Socket connection status' }, Octicon('plug')),
+			a({ href: 'https://github.com/lmtm/copycast' }, Octicon('mark-github'))
+		]),
+		div(select('.hl-themes', hlThemes.map(t => option({ selected: t === hlTheme }, t))))
+	])
+
+export const Resizer = () => div('.resizer')
+
+export const Editor = ({ selected, content }) =>
+	div('.main', [
+		Header({ selected, content }),
+		div('.editor', content
+			? pre(code('.editor-code.hljs', hl(content)))
+			: div('.editor-no-content', '⇐ Select a file on the left'))
+	])
+
+const Header = ({ selected, content }) => {
+	const parts = selected ? selected.split('|') : []
+
+	return header([
+		h2('.crumbs', parts.join(' ❭ ')),
+		selected
+			? a('.download', {
+				download: parts[parts.length - 1],
+				href: toDataUri(content)
+			}, [Octicon('cloud-download'), 'Download file'])
+			: null,
+		selected
+			? a('.clipboard', {
+				attributes: { 'data-clipboard-target': '.editor-code' }
+			}, [Octicon('clippy'), 'Copy file'])
+			: null
+	])
+}
--- client/index.js
+++ client/index.js
@@ -1,21 +1,14 @@
 import { Observable } from 'rx'
 import { run } from '@cycle/core'
-import {
-	a, aside, code, div, footer, header, h1, h2,
-	li, link, option, pre, select, span, ul,
-	makeDOMDriver
-} from '@cycle/dom'
+import { div, link, makeDOMDriver } from '@cycle/dom'
 
+import { Sidebar, Resizer, Editor } from './components'
+import { hlThemes } from './hl'
 import getClickIds$ from './utils/dom'
-import { PATH_SEP, getContent } from './utils/tree-walker'
+import { getContent } from './utils/tree-walker'
 import createSocketIODriver from './drivers/cycle-socket.io'
-import { hl, hlThemes } from './hl'
+
 import Clipboard from 'clipboard'
-
-// protection for files with UTF-8 chars like ❭ in this one
-const toDataUri = (content) =>
-	`data:text/plain;base64,${btoa(unescape(encodeURIComponent(content)))}`
-
 new Clipboard('.clipboard')
 
 function main({ DOM, socket }) {
@@ -81,96 +74,6 @@ function main({ DOM, socket }) {
 }
 
 // components
-
-function Octicon (name) {
-	return span(`.octicon.octicon-${name}`)
-}
-
-function Sidebar ({ tree, selected, collapsed, conn, hlTheme, sidebarWidth }) {
-	return aside('.sidebar', { style: { width: `${sidebarWidth}px` } }, [
-		h1('.logo', a({ href: 'https://github.com/lmtm/copycast' }, 'copycast')),
-		div('.tree',
-			Dir({ root: true, path: tree.name, tree, selected, collapsed })
-		),
-		Footer({ conn, hlTheme })
-	])
-}
-
-function Dir ({ root, path, tree, selected, collapsed }) {
-	if (!tree || !tree.children) return
-
-	path = root ? tree.name : `${path}${PATH_SEP}${tree.name}`
-	let klass = '.dirname'
-	let trees = []
-	if (collapsed.has(path)) {
-		klass += '.collapsed'
-	} else {
-		trees = tree.children.map((child) => {
-			return (child.children)
-				? Dir({ path, tree: child, selected, collapsed })
-				: File({ path, file: child, selected })
-		})
-	}
-	return ul('.dir', [li(klass, { data: { id: path } }, tree.name), ...trees])
-}
-
-function File ({ path, file, selected }) {
-	const { name, updatedAt } = file
-	const id = `${path}${PATH_SEP}${name}`
-	let elapsed = ''
-	if (updatedAt) {
-		let ago = Math.round((Date.now() - updatedAt) / 1000)
-		if (ago <= 180) {
-			elapsed = `${ago}s`
-		}
-	}
-	return li(`.file${ selected === id ? '.selected' : '' }`, [
-		span('.filename', { data: { id } }, name),
-		span('.elapsed', { data: { id } }, elapsed)
-	])
-}
-
-function Footer ({ conn, hlTheme }) {
-	return footer([
-		div('.status', [
-			span(`.conn-${ conn ? 'on' : 'off' }`, { title: 'Socket connection status' }, Octicon('plug')),
-			a({ href: 'https://github.com/lmtm/copycast' }, Octicon('mark-github'))
-		]),
-		div(select('.hl-themes', hlThemes.map(t => option({ selected: t === hlTheme }, t))))
-	])
-}
-
-function Resizer () {
-	return div('.resizer')
-}
-
-function Editor ({ selected, content }) {
-	return div('.main', [
-		Header({ selected, content }),
-		div('.editor', content
-			? pre(code('.editor-code.hljs', hl(content)))
-			: div('.editor-no-content', '⇐ Select a file on the left'))
-	])
-}
-
-function Header ({ selected, content }) {
-	const parts = selected ? selected.split('|') : []
-
-	return header([
-		h2('.crumbs', parts.join(' ❭ ')),
-		selected
-			? a('.download', {
-				download: parts[parts.length - 1],
-				href: toDataUri(content)
-			}, [Octicon('cloud-download'), 'Download file'])
-			: null,
-		selected
-			? a('.clipboard', {
-				attributes: { 'data-clipboard-target': '.editor-code' }
-			}, [Octicon('clippy'), 'Copy file'])
-			: null
-	])
-}
 
 run(main, {
 	DOM: makeDOMDriver('#root'),
