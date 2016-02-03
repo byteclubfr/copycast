3ae5eda0f56591cca75af22e56467df14483a2d7
Delapouite <delapouite@gmail.com>
2016-02-03 10:12:46 +0100

    refactor(client): big main → MVI

--- client/index.js
+++ client/index.js
@@ -15,19 +15,13 @@ new Clipboard('.clipboard')
 // localStorage key
 const LS_HL_THEME = 'copycast.hl-theme'
 
-function main({ DOM, socket, storage }) {
-	// intents
-
-	// to refresh elapsed counter regularly
-	const elapsed$ = Observable.timer(0, 5000)
-
+const intent = ({ DOM, socket, storage }) => {
 	// from server's watcher
 	const tree$ = socket.get('tree')
 	// to add visual indicator
 	const conn$ = Observable.merge(
 		socket.get('connect').map(() => true),
-		socket.get('disconnect').map(() => false)
-	)
+		socket.get('disconnect').map(() => false))
 
 	// sidebar tree
 	const selected$ = getClickIds$(DOM, 'file')
@@ -43,42 +37,39 @@ function main({ DOM, socket, storage }) {
 	const mouseDown$ =  DOM.select('.resizer').events('mousedown')
 	const mouseUp$ =  DOM.select('#app').events('mouseup')
 	const sidebarWidth$ = mouseDown$.flatMap(() =>
-		mouseMove$.map(({ clientX }) => clientX)
-			.takeUntil(mouseUp$)
+		mouseMove$.map(({ clientX }) => clientX).takeUntil(mouseUp$)
 	).startWith(230)
 
 	const hlTheme$ = storage.local.getItem(LS_HL_THEME)
 		.map(v => v === null ? hlThemes[34] : v) // default to GitHub theme
 
-	// model
+	// to refresh elapsed counter regularly
+	const elapsed$ = Observable.timer(0, 5000)
 
-	const state$ = Observable.combineLatest(
+	return { tree$, selected$, collapsed$, conn$, hlTheme$, sidebarWidth$, elapsed$ }
+}
+
+const model = ({ tree$, selected$, collapsed$, conn$, hlTheme$, sidebarWidth$, elapsed$ }) =>
+	Observable.combineLatest(
 		tree$, selected$, collapsed$, conn$, hlTheme$, sidebarWidth$, elapsed$,
 		(tree, selected, collapsed, conn, hlTheme, sidebarWidth) =>
-			({ tree, selected, collapsed, conn, hlTheme, sidebarWidth, content: getContent(tree, selected) })
-	)
+			({ tree, selected, collapsed, conn, hlTheme, sidebarWidth, content: getContent(tree, selected) }))
 
-	// view
+const view = (state$) =>
+	state$.map(({ tree, selected, collapsed, conn, hlTheme, sidebarWidth, content }) =>
+		div('#app', [
+			Sidebar({ tree, selected, collapsed, conn, hlTheme, sidebarWidth }),
+			Resizer(),
+			Editor({ selected, content }),
+			link({ rel: 'stylesheet', href: `hl-themes/${hlTheme}.css` })
+		]))
 
-	const vtree$ = state$.map(
-		({ tree, selected, collapsed, conn, hlTheme, sidebarWidth, content }) =>
-			div('#app', [
-				Sidebar({ tree, selected, collapsed, conn, hlTheme, sidebarWidth }),
-				Resizer(),
-				Editor({ selected, content }),
-				link({ rel: 'stylesheet', href: `hl-themes/${hlTheme}.css` })
-			])
-	)
-
+const main = (sources) => ({
+	DOM: view(model(intent(sources))),
 	// save sidebar <select> value to localStorage
-	const storageInstructions$ = DOM.select('.hl-themes').events('change')
-		.map(ev => ({ key: LS_HL_THEME, value: ev.target.value }))
-
-	return {
-		DOM: vtree$,
-		storage: storageInstructions$
-	}
-}
+	storage: sources.DOM.select('.hl-themes').events('change')
+		.map(({ target }) => ({ key: LS_HL_THEME, value: target.value }))
+})
 
 // components
 
