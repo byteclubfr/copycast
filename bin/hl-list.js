69e92b4c3b0b17a9475537568465b9119bdacb8f
Nicolas Chambrier <naholyr@gmail.com>
2016-04-26 20:04:25 +0200

    fix(hl-themes): fix unknown module list.json

--- bin/hl-list.js
+++ bin/hl-list.js
@@ -3,4 +3,4 @@ import { writeFileSync, readdirSync } from 'fs'
 const files = readdirSync('./client/hl-themes').filter(f => f.match(/\.css$/))
 const json = JSON.stringify(files, null, '  ')
 
-writeFileSync('list.json', json + '\n')
+writeFileSync('./client/hl-themes/list.json', json + '\n')
