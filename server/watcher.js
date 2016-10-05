0dacc0f32d23a0dcb47967b7ec3304f4058233b0
Delapouite <delapouite@gmail.com>
2016-10-05 20:33:05 +0200

    fix(watcher): avoid fatal crash on phantom unlink before change event

--- server/watcher.js
+++ server/watcher.js
@@ -18,8 +18,18 @@ const ignoredPatterns = () => {
 	return ignoreToGlobs('.gitignore')
 }
 
-exports.createWatcher = (root, tree, done) =>
-	chokidar.watch(root, {
+exports.createWatcher = (root, tree, done) => {
+	// unfortunate helper, see "change" event handler below
+	const addFile = (path, content, updatedAt) => {
+		const name = P.basename(path)
+		content = isHugeFile(name, content) ? 'Content too huge' : content
+		const child = createFile(name, content)
+		if (updatedAt) child.updatedAt = updatedAt
+		addChild(getChildren(tree, P.dirname(path)), child)
+		done(tree)
+	}
+
+	return chokidar.watch(root, {
 		ignored: ignoredPatterns() // Get ignored files from standard ignore files
 			// and add static files and files we anyway always want to ignore
 			.concat(['**/.git', '**/*.gif', '**/*.jpg', '**/*.png', '**/*.eot', '**/*.ttf', '**/*.woff']),
@@ -42,15 +52,7 @@ exports.createWatcher = (root, tree, done) =>
 	.on('add', (path) => {
 		debug('+f', path)
 
-		fs.readFile(path, 'utf8', (err, content) => {
-			const name = P.basename(path)
-			content = isHugeFile(name, content) ? 'Content too huge' : content
-			addChild(
-				getChildren(tree, P.dirname(path)),
-				createFile(name, content)
-			)
-			done(tree)
-		})
+		fs.readFile(path, 'utf8', (err, content) => addFile(path, content))
 	})
 	.on('unlinkDir', (path) => {
 		debug('-d', path)
@@ -70,11 +72,18 @@ exports.createWatcher = (root, tree, done) =>
 		fs.readFile(path, 'utf8', (err, content) => {
 			const c = getChildren(tree, P.dirname(path))
 				.find(c => c.name === P.basename(path))
-			c.content = content
-			c.updatedAt = stats.mtime.getTime()
-			done(tree)
+
+			if (c) {
+				c.content = content
+				c.updatedAt = stats.mtime.getTime()
+				done(tree)
+			} else {
+				// chokidar edge case when a "change" is preceded by a phantom "unlink"
+				debug('~f', path)
+				addFile(path, content, Date.now())
+			}
 		})
 	})
 	.on('error', (error) => debug('Watcher error', error))
 	.on('ready', () => debug('Initial scan complete. Ready for changes'))
-
+}
