e596b88f58358f3afdddfeecd8093de873d5b25c
Delapouite <delapouite@gmail.com>
2016-10-22 16:21:46 +0200

    feat(CLI): add --help (minimist → yargs)

--- bin/copycast.js
+++ bin/copycast.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2))
 const copycast = require('../server')
+const argv = require('yargs')
+	.alias('d', 'dir')
+	.describe('d', 'dir to watch')
+	.default('d', '.')
+
+	.alias('p', 'port')
+	.describe('p', 'web port')
+	.default('p', 42000)
+
+	.alias('l', 'localtunnel')
+	.describe('l', 'enable localtunnel (public URL)')
+
+	.describe('G', 'disable git support')
+	.help('h')
+	.alias('h', 'help')
+	.argv
 
 copycast.start({
 	dir: argv.d,
